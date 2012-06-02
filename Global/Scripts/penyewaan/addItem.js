107c1ef46acdeda7984b525d777eaf392dc4987f
Denny Wu <denny.wu13@gmail.com>
2012-06-02 18:52:36 +0700

    select customer and add item

--- Global/Scripts/penyewaan/addItem.js
+++ Global/Scripts/penyewaan/addItem.js
@@ -1,79 +1,16 @@
-﻿var RENTAL = {};
-RENTAL.SELECTCUSTOMER = {};
-RENTAL.SELECTCUSTOMER.sessionTimeOutSearchingCust;
+﻿//var RENTAL = {};
+RENTAL.ITEM = {};
 
-$(document).ready(function () {
-    var selectCust = RENTAL.SELECTCUSTOMER;
-    $("#inputCustomer").keyup(selectCust.searchCustomer);
-    $("#inputCustomer").blur(selectCust.hideSearchedResult);
-    $("#inputCustomer").focus(selectCust.showSearchedResult);
-    $("#searchCustResult .contain")
-});
-
-RENTAL.SELECTCUSTOMER.searchCustomer = function (ev) {
-    window.clearTimeout(RENTAL.SELECTCUSTOMER.sessionTimeOutSearchingCust);
-    if (ev.keyCode == "13") {
-        RENTAL.SELECTCUSTOMER.clearSearchedResult();
-        RENTAL.SELECTCUSTOMER.showLoading();
-        RENTAL.SELECTCUSTOMER.searchingCustomer();
-    }
-    else if (ev.keyCode == "27") {
-        $("#inputCustomer").blur();
-        RENTAL.SELECTCUSTOMER.hideSearchedResult();
-    }
-    else {
-        RENTAL.SELECTCUSTOMER.clearSearchedResult();
-        RENTAL.SELECTCUSTOMER.showLoading();
-        RENTAL.SELECTCUSTOMER.sessionTimeOutSearchingCust = window.setTimeout(RENTAL.SELECTCUSTOMER.searchingCustomer, 1000);
-    }
+RENTAL.ITEM.addRow = function () {
+    $("#tblitem tbody").append("<tr><td><input type='text' name='itemname'/></td><td><textarea></textarea></td>" +
+                        "<td><input type='number' name='qty' class='itemqty' min='1'/></td>" +
+                        "<td><input type='text' name='harga' class='itemharga'/></td>" +
+                        "<td><div><div class='itemtotalCcy'>Rp</div><div class='itemtotalValue'>...</div></div></td>"+
+                        "<td><span id='itemRemove'>X</span></td></tr>");
 }
-RENTAL.SELECTCUSTOMER.searchingCustomer = function () {
-    
-    
-    var key = $("#inputCustomer").val();
-    if (key == "")
-        return RENTAL.SELECTCUSTOMER.clearSearchedResult();
-    $.ajax({
-        type: 'GET',
-        url: '/Customer/SearchCustomerByName?key=' + key,
-        dataType: 'json',
-        success: RENTAL.SELECTCUSTOMER.searchingCustomerCallBack
-    });
+RENTAL.ITEM.showRemoveButton = function () {
+    $("#itemRemove").show();
 }
-RENTAL.SELECTCUSTOMER.searchingCustomerCallBack = function (data) {
-    RENTAL.SELECTCUSTOMER.clearSearchedResult();
-    if (data.length == 0) {
-        RENTAL.SELECTCUSTOMER.showEmptyResult();
-    }
-    else {
-        for (var i = 0; i < data.length; i++) {
-            $("#searchCustResult").append(RENTAL.SELECTCUSTOMER.createSearchedCustRow(data[i]));
-        }
-    }
-}
-RENTAL.SELECTCUSTOMER.createSearchedCustRow = function(cust) {
-    var divContain = $("<div>", { 'class': 'contain' });
-    var divName = $("<div>", { 'class': 'custName', 'text': cust.Name});
-    var divAlamat = $("<div>", { 'class': 'custAlamat', 'text': cust.Alamat });
-    var divEmail = $("<div>", { 'class': 'custEmail', 'text': cust.Email });
-
-    divName.appendTo(divContain);
-    divAlamat.insertAfter(divName);
-    divEmail.insertAfter(divAlamat);
-    return divContain;
-}
-RENTAL.SELECTCUSTOMER.clearSearchedResult = function () {
-    $("#searchCustResult").empty();
-}
-RENTAL.SELECTCUSTOMER.showSearchedResult = function () {
-    $("#searchCustResult").show();
-}
-RENTAL.SELECTCUSTOMER.hideSearchedResult = function () {
-    $("#searchCustResult").hide();
-}
-RENTAL.SELECTCUSTOMER.showLoading = function () {
-    $("#searchCustResult").append("<div align='center' style='color:#457BD9;'>Harap Tunggu...</div>");
-}
-RENTAL.SELECTCUSTOMER.showEmptyResult = function () {
-    $("#searchCustResult").append("<div align='center' style='color:#457BD9;'>Data Tidak Ditemukan</div>");
+RENTAL.ITEM.hideRemoveButton = function () {
+    $("#itemRemove").hide();
 }
---
+++ Global/Scripts/penyewaan/penyewaan.js
@@ -0,0 +1,34 @@
+﻿var RENTAL = {};
+RENTAL.SELECTCUSTOMER = {};
+RENTAL.SELECTCUSTOMER.sessionTimeOutSearchingCust;
+
+$(document).ready(function () {
+    var selectCust = RENTAL.SELECTCUSTOMER;
+    $("#inputCustomer").keyup(selectCust.searchCustomer);
+    $("#inputCustomer").focus(selectCust.showSearchedResult);
+    $("#inputCustomer").blur(function () {
+        setTimeout(selectCust.hideSearchedResult, 200);
+    });
+    RENTAL.setupDatePicker();
+    $("#addRow").click(RENTAL.ITEM.addRow);
+    $("#tblitem tbody tr").hover(RENTAL.ITEM.showRemoveButton);
+    $("#tblitem tbody tr").mouseleave(RENTAL.ITEM.hideRemoveButton);
+});
+
+RENTAL.setupDatePicker = function () {
+    var dates = $("#fromdate, #todate").datepicker({ dateFormat: 'yy-mm-dd',
+        defaultDate: "+1w",
+        changeMonth: true,
+        gotoCurrent: true,
+        numberOfMonths: 1,
+        onSelect: function (selectedDate) {
+            var option = this.id == "fromdate" ? "minDate" : "maxDate",
+					instance = $(this).data("datepicker"),
+					date = $.datepicker.parseDate(
+						instance.settings.dateFormat ||
+						$.datepicker._defaults.dateFormat,
+						selectedDate, instance.settings);
+            dates.not(this).datepicker("option", option, date);
+        }
+    });
+}
