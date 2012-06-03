ae0af6c72e63714b549862bb11ab3140a9e86c3e
Denny Wu <denny.wu13@gmail.com>
2012-06-03 22:57:29 +0700

    model rental listview

---
+++ Global.Repository/models/RentalListViewReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BonaStoco.Inf.Data.ViewModel;
+
+namespace Global.Repository.models
+{
+    [NamedSqlQuery("GetRentalListView",@"SELECT 
+                    h.rentalid,
+                    h.rentalno,
+                    c.name as CustomerName,
+                    h.transactiondate,
+                    h.duedate,
+                    s.total 
+                    FROM tblrentalheader h inner join 
+                    tblcustomer c on h.custid = c.id inner join 
+                    tblrentalsummary s on h.rentalid = s.rentalid")]
+    public class RentalListViewReport : IViewModel
+    {
+        public Guid RentalId { get; set; }
+        public string RentalNo { get; set; }
+        public string CustomerName { get; set; }
+        public string TransactionDate { get; set; }
+        public string DueDate { get; set; }
+        public decimal OutStanding { get; set; }
+        public decimal Total { get; set; }
+    }
+}
